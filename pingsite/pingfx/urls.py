826467ef1cacd6739a23ba7260bc9906b1e65716
vagrant <vagrant@vagrant.vm>
2019-09-16 03:52:17 +0000

    Adding CRUD

--- pingsite/pingfx/urls.py
+++ pingsite/pingfx/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new/<user>/<email>', views.createUser, name='createUser'),
+    path('<user>', views.getEmail, name='getEmail'),
+    path('<user>/edit/<new_email>', views.editEmail, name='editEmail'),
+    path('<user>/delete/', views.deleteUser, name='deleteUser'),
 ]
--- pingsite/pingfx/views.py
+++ pingsite/pingfx/views.py
@@ -7,18 +7,30 @@ from .models import Website, User
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-# def createUser(request, user, email)
+def createUser(request, user, email):
+    newUser = User(user=user, email=email)
+    newUser.save()
+    return HttpResponse("I just created a new user {} with email {}.".format(user, email))
 
 # def createWebsite(request, user, URL)
 
-# def getEmail(request, user)
+def getEmail(request, user):
+    getUser = User.objects.get(user=user)
+    return HttpResponse("The email address for {} is {}.".format(user, getUser.email))
 
 # def getURLs(request, user)
 
 # def editURL(request, user, URL, new_URL)
 
-# def editEmail(request, user, new_email)
+def editEmail(request, user, new_email):
+    editUser = User.objects.get(user=user)
+    editUser.email = new_email
+    editUser.save()
+    return HttpResponse("The email address for {} is now {}.".format(user, editUser.email))    
 
-# def deleteUser(request, user)
+def deleteUser(request, user):
+    deleteUser = User.objects.get(user=user)
+    deleteUser.delete()
+    return HttpResponse("Deleted user {}.".format(user))
 
 # def deleteURL(request, user, URL)
